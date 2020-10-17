35ee052ec8933eaa4743ec36b1a829005276f648
chipa3 <chipa_124578@outlook.com>
2020-10-16 19:04:47 -0600

    Navegador
    Navegador con la DLL de Reporteador sin agregar al objeto comun Navegador

---
+++ PruebaNavegador/Reporteador-master/Reporteador/CapaControlador/ControladoresReporteador/clsControlReporteador.cs
@@ -0,0 +1,42 @@
+﻿using CapaModelo;
+using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaControlador.ControladoresReporteador
+{
+    public class clsControlReporteador
+    {
+        clsSentencia sentencia = new clsSentencia(); // instanciar la clase sentencia
+        clsConexion conexion = new clsConexion(); // instanciar la clase conexion
+        private string sRuta; // variable para la ruta 
+
+        // variable para la buaqueda de la ruta del reporte 
+        public string obtenerRuta(int iID)
+        {
+            try
+            {
+                string sComando = "SELECT ruta_reporte FROM REPORTE WHERE estado_reporte=1 AND pk_id_reporte="+iID;
+                OdbcCommand comando = new OdbcCommand(sComando, conexion.conexion());
+                OdbcDataReader registro = comando.ExecuteReader();
+
+
+                while (registro.Read())
+                {
+                    sRuta= registro["ruta_reporte"].ToString();
+                }
+                return sRuta;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener datos");
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
